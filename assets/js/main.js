627483284ea0d47d9b86d7f544fc30d449aa50f3
KennyAlmeida <kenny_almeida_25@hotmail.com>
2024-04-28 18:51:28 -0300

    fix: atualizando portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,3 +41,25 @@ class MobileNavbar {
     ".nav-list li"
   );
   mobileNavbar.init();
+
+
+  const checkbox = document.getElementById("checkbox")
+checkbox.addEventListener("change", () => {
+  document.body.classList.toggle("white")
+  const header = document.querySelector("header");
+  header.classList.toggle("white");
+})
+
+function typeWrite(elemento){
+  const textoArray = elemento.innerHTML.split('');
+  elemento.innerHTML = ' ';
+  textoArray.forEach(function(letra, i){   
+    
+  setTimeout(function(){
+      elemento.innerHTML += letra;
+  }, 75 * i)
+
+});
+}
+const titulo = document.querySelector('.titulo-profissional');
+typeWrite(titulo);
